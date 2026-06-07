f8500e236b87515417c6bb77995e18f2d4500c90
tt2819ais-arch <tt2819ais@gmail.com>
2026-06-07 19:36:33 +0000

    Void redesign: dark theme, glass cards, gauge, haptics, animations + new app icon

--- NetPulse/App/NetPulseApp.swift
+++ NetPulse/App/NetPulseApp.swift
@@ -2,20 +2,10 @@ import SwiftUI
 
 @main
 struct NetPulseApp: App {
-    @AppStorage("manualTheme") private var manualTheme: String = "system"
-
     var body: some Scene {
         WindowGroup {
             RootTabView()
-                .preferredColorScheme(colorScheme)
-        }
-    }
-
-    private var colorScheme: ColorScheme? {
-        switch manualTheme {
-        case "light": return .light
-        case "dark": return .dark
-        default: return nil
+                .preferredColorScheme(.dark)
         }
     }
 }
